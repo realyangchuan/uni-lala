abfa7cf1da7c535da3871efbc8affb1ee9a41f14
Yang Chuan <realyangchuan@qq.com>
2025-05-14 22:54:07 +0800

    uni.request

---
+++ README.md
@@ -0,0 +1,64 @@
+# uni-lala
+
+一个uniapp请求工具，promisify，类axios，带拦截器
+
+只新增了**1个可选属性**：`baseURL`，作用跟axios的一样
+
+## 快速使用
+
+```js
+import request, { create } from 'uni-lala'
+
+// 基础用法：options跟uni.request完全一致
+await request({ ...options })
+
+// 请求实例：除了uni.request正常参数外，defaultOptions还可以设置`baseURL`，作用跟axios的一样
+const requestInstance = create({ ...defaultOptions })
+
+// 各种类axios简化请求
+request.get(url, data, options)
+request.post(url, data, options)
+request.put(url, data, options)
+...
+
+requestInstance.get(url, data, options)
+requestInstance.post(url, data, options)
+requestInstance.put(url, data, options)
+...
+```
+
+## 拦截器
+
+```js
+// 请求拦截器
+requestInstance.interceptors.request.use(function(options) {
+  // 内部可以异步处理逻辑
+
+  // 锁定
+  // requestInstance.lock()
+  // 解锁
+  // requestInstance.unlock()
+  // 取消锁定
+  // requestInstance.cancel()
+
+  // 返不返回都行，不返回时工具使用options继续发起真正请求
+  return options
+})
+
+// 响应拦截器
+requestInstance.interceptors.response.use(
+  function(response, options) {
+    // 内部可以异步处理逻辑
+
+    // requestInstance.interceptors.response.lock()
+    // requestInstance.interceptors.response.unlock()
+    // requestInstance.interceptors.response.cancel()
+
+    // 返不返回都行
+    return response
+  },
+  function(err, options) {
+    // 异常流程处理，处理后会走reject流程
+  }
+)
+```
---
+++ index.js
@@ -0,0 +1,176 @@
+function handleInterceptor(interceptor) {
+  let _resolve = null
+  let _reject = null
+  interceptor.p = null
+
+  function _reset() {
+    interceptor.p = _resolve = _reject = null
+  }
+
+  function lock() {
+    interceptor.p = new Promise((resolve, reject) => {
+      _resolve = resolve
+      _reject = reject
+    })
+  }
+
+  function unlock() {
+    if (_resolve) {
+      _resolve()
+      _reset()
+    }
+  }
+
+  function cancel() {
+    if (_reject) {
+      _reject(new Error('canceled'))
+      _reset()
+    }
+  }
+
+  Object.assign(interceptor, { lock, unlock, cancel })
+}
+
+function createInterceptors() {
+  const interceptors = {
+    request: {
+      use(handler) {
+        this.handler = handler
+      }
+    },
+    response: {
+      use(handler, errHandler) {
+        this.handler = handler
+        this.errHandler = errHandler
+      }
+    }
+  }
+
+  handleInterceptor(interceptors.request)
+  handleInterceptor(interceptors.response)
+
+  return interceptors
+}
+
+function enqueueIfLocked(p, cb) {
+  if (p) {
+    p.then(async () => await cb?.())
+  } else {
+    cb?.()
+  }
+}
+
+function isPlainObject(obj) {
+  return Object.prototype.toString.call(obj).slice(8, -1) === 'Object'
+}
+
+function mergeOptions(target, source, root = true) {
+  const res = { ...target }
+  for (const k in source) {
+    const baseURL = source['baseURL']
+    if (root && k === 'baseURL' && baseURL) {
+      const reqUrl = res['url']
+      if (typeof reqUrl === 'undefined' || /^https?:\/\//.test(reqUrl)) {
+        continue
+      }
+
+      // 去掉前面的斜线`/`
+      const i = reqUrl.search(/\w/)
+      const path = i === -1 ? '' : reqUrl.slice(i)
+      if (baseURL.endsWith('/')) {
+        res['url'] = baseURL + path
+      } else {
+        res['url'] = [baseURL, path].filter(Boolean).join('/')
+      }
+
+      continue
+    }
+
+    if (res.hasOwnProperty(k)) {
+      if (isPlainObject(res[k]) && isPlainObject(source[k])) {
+        res[k] = mergeOptions(res[k], source[k], false)
+      }
+    } else {
+      res[k] = source[k]
+    }
+  }
+
+  return res
+}
+
+function createInstance(defaultOptions, makeRequest) {
+  const interceptors = createInterceptors()
+
+  function _request(options) {
+    _request.lock = interceptors.request.lock
+    _request.unlock = interceptors.request.unlock
+    _request.cancel = interceptors.request.cancel
+
+    return new Promise((resolve, reject) => {
+      enqueueIfLocked(interceptors.request.p, async () => {
+        const originConfig = mergeOptions(options, defaultOptions)
+        const config =
+          (await Promise.resolve(
+            interceptors.request.handler?.(originConfig)
+          )) ?? originConfig
+        makeRequest(config)
+          .then(response => {
+            enqueueIfLocked(interceptors.response.p, async () => {
+              const res =
+                (await Promise.resolve(
+                  interceptors.response.handler?.(response, config)
+                )) ?? response
+              resolve(res)
+            })
+          })
+          .catch(error => {
+            enqueueIfLocked(interceptors.response.p, async () => {
+              const err =
+                (await Promise.resolve(
+                  interceptors.response.errHandler?.(error, config)
+                )) ?? error
+              reject(err)
+            })
+          })
+      })
+    })
+  }
+
+  ['get', 'post', 'put', 'delete', 'connect', 'head', 'options', 'trace'].forEach(method => {
+    _request[method] = (url, data, options) => _request({
+      url,
+      data,
+      ...options,
+      method: method.toUpperCase()
+    })
+  })
+
+  return _request
+}
+
+function handleRequestApi(requestApi, options) {
+  return new Promise((resolve, reject) => {
+    requestApi({
+      ...options,
+      async success(res) {
+        resolve((await options.success?.(res)) ?? res)
+      },
+      async fail(err) {
+        reject((await options.fail?.(err)) ?? err)
+      },
+      async complete(res) {
+        await options.complete?.(res)
+      }
+    })
+  })
+}
+
+function uniRequest(options) {
+  return handleRequestApi(uni.request, options)
+}
+
+export default createInstance({}, uniRequest)
+
+export function create(defaultOptions = {}) {
+  return createInstance(defaultOptions, uniRequest)
+}
